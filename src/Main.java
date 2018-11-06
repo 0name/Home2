cd09d71bc7efc112914cc5be23c11328a8b4af6a
N1Kit <N9052233291@yandex.ru>
2018-11-06 17:30:48 +0300

    Main was created

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        homeWork.task1(8.5, 11.45);
+    }
+}
--- src/homeWork.java
+++ src/homeWork.java
@@ -1,8 +1,4 @@
 public class homeWork {
-    public static void main(String[] args) {
-        task1(8.5, 11.45);
-    }
-
 //        1) Создать программу, выводящую на экран ближайшее к 10 из двух чисел, записанных в переменные m и n.
 //      Например, среди чисел 8.5 и 11.45 ближайшее к десяти 11.45.
     public static void task1(double first, double second) {

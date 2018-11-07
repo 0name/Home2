725905016e3cf6566b204f38a50a281f87266492
Dan11l <noname.spb@mail.ru>
2018-11-07 06:29:18 +0300

    не доделал

--- src/homeWork.java
+++ src/homeWork.java
@@ -1,6 +1,7 @@
 public class homeWork {
     public static void main(String[] args) {
         task1(8.5, 11.45);
+        task2(2,2,2);
     }
 
 //        1) Создать программу, выводящую на экран ближайшее к 10 из двух чисел, записанных в переменные m и n.
@@ -16,5 +17,14 @@ public class homeWork {
 //      2) В три переменные a, b и c записаны три вещественных числа.
 //      Создать программу, которая будет находить и выводить на экран вещественные
 //      корни квадратного уравнения ax²+bx+c=0, либо сообщать, что корней нет.
+    public static void task2(float var1, float var2, float var3){
+    float a = var1, b = var2, c = var3, d, x1, x2;
+    d = (float) Math.sqrt(b * b) - 4 * a * c ;
+    if (d > 0) {
+        x1 = (-b + d) / a * 2;
+        x2 = (-b - d) / a * 2;
+        System.out.println("x1 = %.2f " + x1 + " x2 = %.0f" + x2);
+    }else
+    }
 
 }

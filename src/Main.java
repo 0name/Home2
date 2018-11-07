a9c6a0e06f6ba6107ddc702cf662c8ebb151f96f
Dan11l <noname.spb@mail.ru>
2018-11-07 06:40:32 +0300

    I've done it

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        homeWork.task1(8.5, 11.45);
+        homeWork.task2(1,4,2);
+    }
+}
--- src/homeWork.java
+++ src/homeWork.java
@@ -1,8 +1,4 @@
 public class homeWork {
-    public static void main(String[] args) {
-        task1(8.5, 11.45);
-        task2(2,2,2);
-    }
 
 //        1) Создать программу, выводящую на экран ближайшее к 10 из двух чисел, записанных в переменные m и n.
 //      Например, среди чисел 8.5 и 11.45 ближайшее к десяти 11.45.
@@ -19,12 +15,13 @@ public class homeWork {
 //      корни квадратного уравнения ax²+bx+c=0, либо сообщать, что корней нет.
     public static void task2(float var1, float var2, float var3){
     float a = var1, b = var2, c = var3, d, x1, x2;
-    d = (float) Math.sqrt(b * b) - 4 * a * c ;
-    if (d > 0) {
+    d = (float) Math.sqrt((b * b) - 4 * a * c) ;
+    if (d >= 0) {
         x1 = (-b + d) / a * 2;
         x2 = (-b - d) / a * 2;
-        System.out.println("x1 = %.2f " + x1 + " x2 = %.0f" + x2);
+        System.out.printf("x1 = %.2f x2 = %.2f", x1,  x2);
     }else
+        System.out.println("Корней нет");
     }
 
 }

cbd29349b5a3196b3a45e12b0ed7d47ac412286b
Dan11l <noname.spb@mail.ru>
2018-11-10 06:33:26 +0300

    task5

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         homeWork.task1(8.5, 11.45);
         homeWork.task2(1,4,2);
-        homeWork.task3(774);
-        homeWork.task4(3, 9, -1);
+        homeWork.task3(768);
+        homeWork.task4(7, 0, -5);
+        homeWork.task5();
     }
 }
